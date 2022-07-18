c80026eeec10a30b850e72f0c8e091f2b7e4b52a
Sergey Yakimov <syakimov@fun.co>
2022-07-18 14:02:44 +0300

    validation

--- src/main/lessons/FuncLocal.kt
+++ src/main/lessons/FuncLocal.kt
@@ -6,6 +6,7 @@ fun main() {
     println(oneLineCoube(3))
     println("Max age 18, 30: "+ maxAge(18, 30))
     println("Max age -2, -5: "+ maxAge(-2, -5))
+    println("Max age 110, 120: "+ maxAge(110, 120))
 }
 
 fun oneLineCoube(x: Int) = x * x * x
