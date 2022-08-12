6e9269a9c267c2c6dc52b249715f19a29c8fbd2b
Sergey Yakimov <syakimov@fun.co>
2022-08-12 12:22:25 +0300

    let

--- src/main/lessons/scope/let.kt
+++ src/main/lessons/scope/let.kt
@@ -5,14 +5,21 @@ import main.lessons.oop.User
 fun main() {
     val sam = User("Sam", "sam@gmail.com")
 
-    sam.email?.let{
+    sam.email?.let {
         println("Email: $it")      // Email: sam@gmail.com
     }
-    if (sam.email!=null)
+    if (sam.email != null)
         println("Email: ${sam.email}") // аналог без функции let
 
     val tom = User("Tom", null)
-    tom.email?.let{
+    tom.email?.let {
         println("Email: $it")  // функция let не будет выполняться
     }
+
+    sam.email?.let(::println)   //sam@gmail.com
+    sam.email?.let(::printEmail) //Email: sam@gmail.com
+}
+
+fun printEmail(email: String) {
+    println("Email: $email")
 }
