20500f36fa471cf37ea00ea3e2fd9f79245aa23e
Sergey Yakimov <syakimov@fun.co>
2022-11-10 22:09:13 +0200

    when

--- src/main/lessons/When.kt
+++ src/main/lessons/When.kt
@@ -4,9 +4,18 @@ fun main() {
     val validNumbers = arrayOf(11, 12, 13)
     val x = 12
     when (x) {
-        in 1..10 -> print("x is in the range")
-        in validNumbers -> print("x is valid")
-        !in 11..20 -> print("x is outside the range")
-        else -> print("none of the above")
+        in 1..10 -> println("x is in the range")
+        in validNumbers -> println("x is valid")
+        !in 11..20 -> println("x is outside the range")
+        else -> println("none of the above")
     }
+
+    val y = when (x) {
+        in 1..10 -> 1
+        in validNumbers -> 2
+        !in 11..20 -> 3
+        else -> 4
+    }
+
+    println(y) // 2
 }
